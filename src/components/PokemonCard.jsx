df6a7cf3a132b3391a51b60e983f96302331342a
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-16 10:28:48 -0700

    updated

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,9 +5,9 @@ import axios from "axios";
 import background from "../assets/bg.webp";
 import pokeBallEmpty from "../assets/pokeballEmpty.png";
 import pokeBallFull from "../assets/pokeballFull.png";
-import oneStar from "../assets/1Star.png";
-import twoStar from "../assets/2Star.png";
-import threeStar from "../assets/3Star.png";
+import oneStar from "../assets/oneStar.png";
+import twoStar from "../assets/twoStar.png";
+import threeStar from "../assets/threeStar.png";
 import ballOpen from "../assets/pokeballOpen.png";
 import ballClosed from "../assets/pokeballClosed2.png";
 import "../styles/pokemonCard.css";
@@ -34,6 +34,7 @@ export function PokemonCard({
   const [moveDetailList2, setMoveDetailList2] = useState([]);
   const [isInfoDivShown, setIsInfoDivShown] = useState(false);
   const types = pokemon.types.map((type, index) => type.type.name);
+  const randId = crypto.randomUUID();
 
   useMemo(() => {
     const moves = moveList?.splice(0, 2);
@@ -67,144 +68,136 @@ export function PokemonCard({
 
   return (
     <>
-      {pokemon !== null ? (
-        <>
-          <div className="cardContainer">
-            <div className="titleContainer">
-              <div className="nameStarContainer">
-                <img
-                  className="starImg"
-                  src={
-                    (starRating == 1 && oneStar) ||
-                    (starRating == 2 && twoStar) ||
-                    (starRating === 3 && threeStar)
-                  }
-                  width="40"
-                  height="40"
-                />
-                {pokemon
-                  ? `${pokemon?.name?.charAt(0).toUpperCase()}${pokemon?.name
-                      ?.slice(1)
-                      .toLowerCase()}`
-                  : "Loading..."}
-              </div>
-              <div className="hpContainer">
-                <div
-                  className="typeContainer"
-                  onMouseEnter={() => setIsInfoDivShown(true)}
-                  onMouseLeave={() => setIsInfoDivShown(false)}
-                >
-                  {typeList.filter((type) =>
-                    types.includes(type.props.className)
-                  )}
-                  <div
-                    className={` ${
-                      isInfoDivShown
-                        ? "typeInfoContainer"
-                        : "invisTypeInfoContainer"
-                    }`}
-                  >
-                    {isInfoDivShown && (
-                      <>
-                        <p>
-                          {types.map((value, index, array) => {
-                            if (index === array.length - 1) {
-                              return value;
-                            } else {
-                              return value + "/";
-                            }
-                          })}
-                        </p>
-                        <span>&nbsp;</span>
-                        <p>type</p>
-
-                        <div
-                          className={`${isInfoDivShown ? "triangle" : ""}`}
-                        ></div>
-                      </>
-                    )}
-                  </div>
-                </div>
-                <div className="hpContainer">
-                  {pokemon?.stats[0].base_stat}
-                  {pokemon?.stats[0].stat.name}
-                </div>
-              </div>
-            </div>
-
-            <div className="pokemonImgContainer">
-              <div className="pokemonImg">
-                {captured && isPokeballShown ? (
-                  <div className="pokeballSprite">
-                    <img src={ballClosed} width="80" />
-                  </div>
-                ) : ballHit === true && ballSpin === false ? (
-                  <div className="pokemonSprite">
-                    {isShiny ? (
-                      <img src={pokemon?.sprites?.front_shiny} />
-                    ) : (
-                      <img src={pokemon?.sprites?.front_default} />
-                    )}
-                  </div>
-                ) : ballHit ? (
-                  <div className="pokeballSprite">
-                    <img src={ballOpen} width="80" className="wiggle" />
-                  </div>
-                ) : (
+      <div className="cardContainer">
+        <div className="titleContainer">
+          <div className="nameStarContainer">
+            <img
+              className="starImg"
+              src={
+                (starRating == 1 && oneStar) ||
+                (starRating == 2 && twoStar) ||
+                (starRating === 3 && threeStar)
+              }
+              width="40"
+              height="40"
+            />
+            {pokemon
+              ? `${pokemon?.name?.charAt(0).toUpperCase()}${pokemon?.name
+                  ?.slice(1)
+                  .toLowerCase()}`
+              : "Loading..."}
+          </div>
+          <div className="hpContainer">
+            <div
+              className="typeContainer"
+              onMouseEnter={() => setIsInfoDivShown(true)}
+              onMouseLeave={() => setIsInfoDivShown(false)}
+            >
+              {typeList.filter((type) => types.includes(type.props.className))}
+              <div
+                className={` ${
+                  isInfoDivShown
+                    ? "typeInfoContainer"
+                    : "invisTypeInfoContainer"
+                }`}
+              >
+                {isInfoDivShown && (
                   <>
-                    <div className="pokemonSprite">
-                      {isShiny ? (
-                        <img src={pokemon?.sprites?.front_shiny} />
-                      ) : (
-                        <img src={pokemon?.sprites?.front_default} />
-                      )}
-                    </div>
+                    <p>
+                      {types.map((value, index, array) => {
+                        if (index === array.length - 1) {
+                          return value;
+                        } else {
+                          return value + "/";
+                        }
+                      })}
+                    </p>
+                    <span>&nbsp;</span>
+                    <p>type</p>
+
+                    <div
+                      className={`${isInfoDivShown ? "triangle" : ""}`}
+                    ></div>
                   </>
                 )}
-
-                <div className="pokemonBackgroundImg">
-                  <img src={background} />
-                </div>
               </div>
             </div>
-
-            <div className="pokemonStatsContainer">
-              <div className="pokemonNameContainer">
-                <div className="pokemonMovesContainer">
-                  {moveDetailList1?.map((move) => (
-                    <div className="moves" key={`move_1_id_${move.id}`}>
-                      <div>{move.name}</div>
-                      PP: {move.pp}
-                    </div>
-                  ))}
-                  {moveDetailList2?.map((move) => (
-                    <div className="moves" key={`move_2_id_${move.id}`}>
-                      <div>{move.name}</div>
-                      PP: {move.pp}
-                    </div>
-                  ))}
-                </div>
-                <div className="shiny">{isShiny ? "Shiny" : ""}</div>
-                <div className="pokedexLink">
-                  <button className="pokedexBtn">Pokedex</button>
-
-                  <img
-                    className={`${ballSpin ? "rotatingPokeball" : ""}`}
-                    onClick={() => {
-                      setIsReleased(!isReleased);
-                      handleRelease(pokeKey);
-                    }}
-                    src={captured ? pokeBallFull : pokeBallEmpty}
-                    width="40"
-                  />
-                </div>
-              </div>
+            <div className="hpContainer">
+              {pokemon?.stats[0].base_stat}
+              {pokemon?.stats[0].stat.name}
             </div>
           </div>
-        </>
-      ) : (
-        <div>Search for your first Pokemon!</div>
-      )}
+        </div>
+
+        <div className="pokemonImgContainer">
+          <div className="pokemonImg">
+            {captured && isPokeballShown ? (
+              <div className="pokeballSprite">
+                <img src={ballClosed} width="80" />
+              </div>
+            ) : ballHit === true && ballSpin === false ? (
+              <div className="pokemonSprite">
+                {isShiny ? (
+                  <img src={pokemon?.sprites?.front_shiny} />
+                ) : (
+                  <img src={pokemon?.sprites?.front_default} />
+                )}
+              </div>
+            ) : ballHit ? (
+              <div className="pokeballSprite">
+                <img src={ballOpen} width="80" className="wiggle" />
+              </div>
+            ) : (
+              <>
+                <div className="pokemonSprite">
+                  {isShiny ? (
+                    <img src={pokemon?.sprites?.front_shiny} />
+                  ) : (
+                    <img src={pokemon?.sprites?.front_default} />
+                  )}
+                </div>
+              </>
+            )}
+
+            <div className="pokemonBackgroundImg">
+              <img src={background} />
+            </div>
+          </div>
+        </div>
+
+        <div className="pokemonStatsContainer">
+          <div className="pokemonNameContainer">
+            <div className="pokemonMovesContainer">
+              {moveDetailList1?.map((move, index) => (
+                <div className="moves" key={`index_${index}`}>
+                  <div>{move.name}</div>
+                  PP: {move.pp}
+                </div>
+              ))}
+              {moveDetailList2?.map((move, index) => (
+                <div className="moves" key={index}>
+                  <div>{move.name}</div>
+                  PP: {move.pp}
+                </div>
+              ))}
+            </div>
+            <div className="shiny">{isShiny ? "Shiny" : ""}</div>
+            <div className="pokedexLink">
+              <button className="pokedexBtn">Pokedex</button>
+
+              <img
+                className={`${ballSpin ? "rotatingPokeball" : ""}`}
+                onClick={() => {
+                  setIsReleased(!isReleased);
+                  handleRelease(pokeKey);
+                }}
+                src={captured ? pokeBallFull : pokeBallEmpty}
+                width="40"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
