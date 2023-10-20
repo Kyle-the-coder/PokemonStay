eee4a4565d87a382b6df4b8e9a4b771628534357
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-20 12:29:06 -0700

    updated

--- src/pages/NewPokemon.jsx
+++ src/pages/NewPokemon.jsx
@@ -7,9 +7,9 @@ import { getRandomNum } from "../api/getRandomNum";
 import { v4 as uuidv4 } from "uuid";
 import { handleRun } from "../functions/handleRun";
 import { useTheme } from "../context/ThemeContext";
-import { pokeballHandleCapture } from "../functions/pokeBallHandleCapture";
-import { greatballHandleCapture } from "../functions/greatballHandleCapture";
-import { ultraballHandleCapture } from "../functions/ultraballHandleCapture";
+import { handlePokeballThrow } from "../functions/handlePokeballThrow";
+import { handleGreatballThrow } from "../functions/handleGreatballThrow";
+import { handleUltraballThrow } from "../functions/handleUltraballThrow";
 import leaf from "../assets/leaf2.png";
 import lgLeaf from "../assets/leaf3.png";
 import lgLeaf2 from "../assets/lgLeaf2.png";
@@ -90,67 +90,42 @@ function NewPokemon() {
   }
   //HANDLE POKEBALL THROW FUNCTION
   function handleBallThrown(pokeInfo) {
-    //MAKE SURE BALLHIT IS NULL AND GATHER INFO
-    setBallHit(null);
-    const count = localStorage.getItem("pokeballCount");
-    const ballHitRand = getRandomNum();
-
-    //HANDLE POKEBALL THROW IF USER HAS NO POKEBALLS
-    if (count <= 0) {
-      setCatchMessage("You ran out of pokeballs");
-      setPokeBallCount(0);
-      localStorage.setItem("pokeballCount", pokeBallCount);
-      handleRun(pokemon);
-      //HANDLE POKEBALL THROW IF USER HAS POKEBALLS
-    } else if (count > 0) {
-      if (pokeballType === pokeBall) {
-        //LOWER POKEBALL COUNT BY 1
-        setPokeBallCount(pokeBallCount - 1);
-        localStorage.setItem("pokeballCount", pokeBallCount);
-      } else if (pokeballType === greatBall) {
-        //LOWER GREATBALL COUNT BY 1
-        setGreatBallCount(greatBallCount - 1);
-        localStorage.setItem("greatballCount", greatBallCount);
-      } else if (pokeballType === ultraBall) {
-        //LOWER ULTRABALL COUNT BY 1
-        setUltraBallCount(ultraBallCount - 1);
-        localStorage.setItem("ultraballCount", ultraBallCount);
-      }
-
-      //START THE ANIMATION
-      setBallSpin(true);
-      setIsBallThrown(true);
-      //TIMEOUT TO ALLOW TIME TO PASS FOR ANIMATION
-      setTimeout(() => {
-        //BALL HIT SUCCESSFULLY
-        if (ballHitRand >= 0 && ballHitRand <= 7) {
-          //LET USER KNOW THE BALL HAS HIT THE POKEMON
-          setBallHit(true);
-          setIsBallThrown(false);
-          //TIMEOUT TO SEE IF POKEMON GETS CAPTURED SUCCESSFULLY
-          setTimeout(() => {
-            if (pokeballType === pokeBall) {
-              pokeballHandleCapture(pokeInfo, pokeballType);
-            } else if (pokeballType === greatBall) {
-              greatballHandleCapture(pokeInfo, pokeballType);
-            } else if (pokeballType === ultraBall) {
-              ultraballHandleCapture(pokeInfo, pokeballType);
-            }
-            // handleCapture(pokeInfo, pokeballType);
-            //IF POKEMON GOT AWAY
-            setCatchMessage("Pokemon got away");
-            setTimeout(() => {
-              setBallSpin(false);
-            }, [1000]);
-          }, [4000]);
-          //BALL HIT UNSUCCESSFULLY
-        } else if (ballHitRand > 7 && ballHitRand <= 10) {
-          //SET EVERYTHING TO FALSE FOR UNSUCCESSFUL THROW
-          setIsBallThrown(false);
-          setBallHit(false);
-          setBallSpin(false);
-        }
-      }, [1000]);
+    if (pokeballType === pokeBall) {
+      handlePokeballThrow(
+        setBallHit,
+        setBallSpin,
+        setCatchMessage,
+        setIsBallThrown,
+        setPokeBallCount,
+        pokeBallCount,
+        pokeInfo,
+        pokeballType,
+        pokemon
+      );
+    } else if (pokeballType === greatBall) {
+      handleGreatballThrow(
+        setBallHit,
+        setBallSpin,
+        setCatchMessage,
+        setIsBallThrown,
+        setGreatBallCount,
+        greatBallCount,
+        pokeInfo,
+        pokeballType,
+        pokemon
+      );
+    } else if (pokeballType === ultraBall) {
+      handleUltraballThrow(
+        setBallHit,
+        setBallSpin,
+        setCatchMessage,
+        setIsBallThrown,
+        setUltraBallCount,
+        ultraBallCount,
+        pokeInfo,
+        pokeballType,
+        pokemon
+      );
     }
   }
   //FUNCTION FOR GETTING MORE POKEBALLS
