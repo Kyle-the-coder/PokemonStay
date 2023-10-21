9462c0e822d5060a2eb58ab175152fcaa5374eea
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-21 13:48:27 -0700

    updated

---
+++ src/components/ImgObject.js
@@ -0,0 +1,33 @@
+import pokeBall from "../assets/pokeballFinal.png";
+import greatBall from "../assets/greatballFinal.png";
+import ultraBall from "../assets/ultraballFinal.png";
+import leaf from "../assets/leaf2.png";
+import lgLeaf from "../assets/leaf3.png";
+import lgLeaf2 from "../assets/lgLeaf2.png";
+import rLeaf from "../assets/reverseLeaf2.png";
+import rLgLeaf from "../assets/reverseLgLeaf.png";
+import wildGrass from "../assets/wildGrass3.png";
+import wildGrass2 from "../assets/wildGrass.png";
+import shinyImg from "../assets/stars.png";
+import rarityImg from "../assets/threeStar.png";
+import pikachu from "../assets/pikachu.png";
+import squirtle from "../assets/squirtle.png";
+import charmander from "../assets/charmander.png";
+
+export const Imgs = {
+  pokeBall: pokeBall,
+  greatBall: greatBall,
+  ultraBall: ultraBall,
+  leaf: leaf,
+  lgLeaf: lgLeaf,
+  lgLeaf2: lgLeaf2,
+  rLeaf: rLeaf,
+  rLgLeaf: rLgLeaf,
+  wildGrass: wildGrass,
+  wildGrass2: wildGrass2,
+  shinyImg: shinyImg,
+  rarityImg: rarityImg,
+  pikachu: pikachu,
+  squirtle: squirtle,
+  charmander: charmander,
+};
