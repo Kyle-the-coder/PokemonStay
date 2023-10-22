c1e73fd152d6893e430ee073796cc813ff34a1f7
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-22 11:28:40 -0700

    updated

---
+++ src/components/PokeballAmountDisplay.jsx
@@ -0,0 +1,52 @@
+import { useTheme } from "../context/ThemeContext";
+
+export default function PokeballAmountDisplay() {
+  const { darkMode, toggleDarkMode } = useTheme();
+  return (
+    <div
+      className={
+        darkMode ? "pokeballDisplayInfoDark" : "pokeballDisplayInfoLight"
+      }
+    >
+      <img src={Imgs.squirtle} width="50px" />
+
+      <h3>Choose your pokeball:</h3>
+
+      <div className="pokeballAndName">
+        <p>Pokeball:</p>
+        <img
+          src={pokeBall}
+          width="50"
+          className="pokeballPng"
+          onClick={() => setPokeballType(pokeBall)}
+        />
+      </div>
+      <div className="pokeballAndName">
+        <p>Greatball:</p>
+        <img
+          src={greatBall}
+          width="50"
+          className="pokeballPng"
+          onClick={() => setPokeballType(greatBall)}
+        />
+      </div>
+      <div className="pokeballAndName">
+        <p>Ultraball:</p>
+        <img
+          src={ultraBall}
+          width="50"
+          className="pokeballPng"
+          onClick={() => setPokeballType(ultraBall)}
+        />
+      </div>
+      <div className="currentChoice">
+        Current Choice:
+        <img
+          src={pokeballType}
+          width="50"
+          className={ballSpin ? "rotatingPokeball" : ""}
+        />
+      </div>
+    </div>
+  );
+}
