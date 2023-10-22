711bf5295737f84ddefbe1a41272b1c8311250bb
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-22 12:08:26 -0700

    updated

--- src/functions/handleRun.js
+++
@@ -1,9 +0,0 @@
-import { getRandomNum } from "../api/getRandomNum";
-import { v4 as uuidv4 } from "uuid";
-
-export function handleRun(pokemon) {
-  localStorage.setItem("pokemon", null);
-  const newInfo = localStorage.getItem("pokemon");
-  const getInfo = JSON.parse(newInfo);
-  return (pokemon = getInfo);
-}
