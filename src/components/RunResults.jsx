a5b37a7c0c0c66b6f14efa659ceb98f3788208de
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-22 11:44:28 -0700

    updated

--- src/components/RunResults.jsx
+++ src/components/RunResults.jsx
@@ -1,16 +1,10 @@
 import CaptureDisplay from "./CaptureDisplay";
 import { EmptyCard } from "./EmptyCard";
 import GameInfoDisplay from "./GameInfoDisplay";
+import PokeballAmountDisplay from "./PokeballAmountDisplay";
 import PokeBallDisplay from "./PokeBallDisplay";
 
-export default function RunResults({
-  catchMessage,
-  pokeBallCount,
-  handleGetMorePokeballs,
-  pokeballType,
-  setPokeballType,
-  ballSpin,
-}) {
+export default function RunResults({ catchMessage }) {
   return (
     <>
       <div className="resultsContainer">
@@ -18,22 +12,10 @@ export default function RunResults({
           <h1 className="textWhite">{catchMessage}</h1>
         </div>
 
-        {/* {pokeBallCount === 0 ? (
-          <>
-            <button
-              className="smallBtn"
-              onClick={() => handleGetMorePokeballs()}
-            >
-              Get More Pokeballs
-            </button>
-          </>
-        ) : (
-          ""
-        )} */}
         <div className="uiContainerRun">
           <GameInfoDisplay />
           <EmptyCard />
-          <PokeBallDisplay />
+          <PokeballAmountDisplay />
         </div>
       </div>
     </>
