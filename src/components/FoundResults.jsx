19251329f26c918445af190b825b3ea249a6e67f
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-22 10:52:40 -0700

    updated

--- src/components/FoundResults.jsx
+++ src/components/FoundResults.jsx
@@ -1,22 +1,23 @@
 export default function FoundResults({ pokemon }) {
   return (
     <>
-      <div className="textWhite">{pokemon.shiny ? <h1>Wow!</h1> : ""}</div>
-      <h1 className="textWhite">
-        {pokemon.captured.capture === true
-          ? `You Caught a wild ${pokemon.shiny ? "Shiny " : ""}
-    ${
-      pokemon?.pokeInfo.name.charAt(0).toUpperCase() +
-      pokemon?.pokeInfo.name.slice(1).toLowerCase()
-    }
-    ! `
-          : `You Found a wild ${pokemon.shiny ? "Shiny " : ""}
-    ${
-      pokemon?.pokeInfo.name.charAt(0).toUpperCase() +
-      pokemon?.pokeInfo.name.slice(1).toLowerCase()
-    }
-    ! `}
-      </h1>
+      {pokemon.captured.capture === true ? (
+        <h1 className="textWhite">
+          You Caught a wild{" "}
+          {pokemon.shiny ? <span className="goldText">Shiny </span> : ""}
+          {pokemon?.pokeInfo.name.charAt(0).toUpperCase() +
+            pokemon?.pokeInfo.name.slice(1).toLowerCase()}
+          {pokemon.shiny ? "!" : ""}
+        </h1>
+      ) : (
+        <h1 className="textWhite">
+          You Found a wild{" "}
+          {pokemon.shiny ? <span className="goldText">Shiny </span> : ""}{" "}
+          {pokemon?.pokeInfo.name.charAt(0).toUpperCase() +
+            pokemon?.pokeInfo.name.slice(1).toLowerCase()}
+          {pokemon.shiny ? "!" : ""}{" "}
+        </h1>
+      )}
     </>
   );
 }
