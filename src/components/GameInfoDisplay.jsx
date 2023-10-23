d6d33d8bcd59c52751bea667ee108155b7874b87
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-23 10:50:17 -0700

    updated

--- src/components/GameInfoDisplay.jsx
+++ src/components/GameInfoDisplay.jsx
@@ -14,13 +14,15 @@ export default function GameInfoDisplay() {
     >
       <img src={Imgs.squirtle} width="50px" />
       <div className="gameInfoDisplayInfo">
-        <h4>Welcome To Pokemon Stay!</h4>
+        <h4>How to catch new Pokemon!</h4>
         <p>
           Here you can search for a pokemon's name at the top bar. You have a
           small chance for a shiny, once a pokemon is found you can pick your
           pokeball type. The higher the pokeball type the better chance you have
-          to catch the pokemon. You have a limited amount of pokeballs so choose
-          wisely! Good luck and CATCH THEM ALL!
+          to catch the pokemon. You have a limited amount of pokeballs per catch
+          session so choose wisely! If you need more pokeballs you must run from
+          your current find, click on the ball you need and get more. Good luck
+          and CATCH THEM ALL!
         </p>
       </div>
     </div>
