f78abcdb6aaca6f8569257f142f5a449e4a1bb6c
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-24 10:32:38 -0700

    updated

---
+++ src/components/StorageLeafDecor.jsx
@@ -0,0 +1,34 @@
+export default function StorageLeafDecor() {
+  return (
+    <>
+      <div className="newPLeaf">
+        <img src={leaf} width="270" />
+      </div>
+      <div className="newPLgLeaf2">
+        <img src={lgLeaf2} width="300" />
+      </div>
+      <div className="newPLgLeaf">
+        <img src={lgLeaf} width="600" />
+      </div>
+      <div className="newPReverseLeaf">
+        <img src={rLeaf} width="460" />
+      </div>
+      <div className="newPReverseLgLeaf">
+        <img src={rLgLeaf} width="350" />
+      </div>
+
+      <div className="wildGrassDark1">
+        <img src={wildGrass2} width="388" />
+      </div>
+      <div className="wildGrassDark2">
+        <img src={wildGrass2} width="388" />
+      </div>
+      <div className="wildGrass1">
+        <img src={wildGrass} width="500" />
+      </div>
+      <div className="wildGrass2">
+        <img src={wildGrass} width="500" />
+      </div>
+    </>
+  );
+}
