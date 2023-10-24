35362cf47a1981dadd39b0d2faff9b0c980fe78a
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-24 10:59:43 -0700

    updated

---
+++ src/components/StorageInfoDisplay.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+export default function StorageInfoDisplay() {
+  return (
+    <div className="storageInfoContainer">
+      <div className="storageInfo">
+        <h1>Welcome to your Storage</h1>
+        <p>
+          Here your pokemon cards will be stored and you will be able to view
+          and filter all your new pokemon as you catch them! Try and catch a
+          pokemon to see for yourself!
+        </p>
+      </div>
+    </div>
+  );
+}
