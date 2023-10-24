785fd3feb4c6ce8b89057ea20c30bdfd64fca185
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-24 10:18:59 -0700

    updated

---
+++ src/components/HomeLeafDecor.jsx
@@ -0,0 +1,23 @@
+import leaf from "../assets/leaf2.png";
+import lgLeaf from "../assets/leaf3.png";
+import rLeaf from "../assets/reverseLeaf2.png";
+import rLgLeaf from "../assets/reverseLgLeaf.png";
+
+export default function HomeLeafDecor() {
+  return (
+    <>
+      <div className="welcomeLeaf">
+        <img src={leaf} width="300" />
+      </div>
+      <div className="welcomeLgLeaf">
+        <img src={lgLeaf} width="700" />
+      </div>
+      <div className="welcomeReverseLeaf">
+        <img src={rLeaf} width="700" />
+      </div>
+      <div className="welcomeReverseLgLeaf">
+        <img src={rLgLeaf} width="400" />
+      </div>
+    </>
+  );
+}
