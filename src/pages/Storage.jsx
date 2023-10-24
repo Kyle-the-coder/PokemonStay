74dad4db0476ddaf06ad45d50b51b22df33f66ec
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-24 10:59:50 -0700

    updated

--- src/pages/Storage.jsx
+++ src/pages/Storage.jsx
@@ -1,30 +1,42 @@
 import { useState } from "react";
 import { useNavigation } from "react-router-dom";
 import { PokeList } from "../components/PokeList";
-import pokeBall from "../assets/pokeballFinal.png";
+import StorageInfoDisplay from "../components/StorageInfoDisplay";
+import StorageLeafDecor from "../components/StorageLeafDecor";
 import "../styles/storage.css";
+import "../styles/storageLeafDecor.css";
 
 function Storage() {
   const { state } = useNavigation();
   const [isCaptured, setIsCaptured] = useState(false);
   const [isReleased, setIsReleased] = useState(false);
   const [isShiny, setIsShiny] = useState(false);
+  const [pokeList, setPokeList] = useState(() => {
+    const list = localStorage.getItem("captureList");
+    if (list === null) return null;
+    return JSON.parse(list);
+  });
 
   return (
     <>
       <div className="container">
         <div className="storageContainer">
+          <StorageLeafDecor />
           <div className="storageTitleContainer">
-            <h1>Storage</h1>
+            <h1 className="z3">Storage</h1>
           </div>
-          <PokeList
-            isCaptured={isCaptured}
-            setIsCaptured={setIsCaptured}
-            state={state}
-            isShiny={isShiny}
-            isReleased={isReleased}
-            setIsReleased={setIsReleased}
-          />
+          {pokeList === null ? (
+            <StorageInfoDisplay />
+          ) : (
+            <PokeList
+              isCaptured={isCaptured}
+              setIsCaptured={setIsCaptured}
+              state={state}
+              isShiny={isShiny}
+              isReleased={isReleased}
+              setIsReleased={setIsReleased}
+            />
+          )}
         </div>
       </div>
     </>
