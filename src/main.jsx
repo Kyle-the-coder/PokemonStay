6f0e1d5e26c04807d38c4778222484faa24d483e
Kyle Mitchell <kylethacoder@gmail.com>
2023-10-29 11:07:02 -0700

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
+import { CardProvider } from "./context/CardContext";
 import { router } from "./routes/router";
 import "./styles/utils.css";
 import "./styles/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
+    <CardProvider>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    </CardProvider>
   </ThemeProvider>
 );
