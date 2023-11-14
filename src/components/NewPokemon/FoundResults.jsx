85940e78a61ae138556f01d3878df7290bc39a80
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-14 10:34:24 -0800

    updated

---
+++ src/components/NewPokemon/FoundResults.jsx
@@ -0,0 +1,23 @@
+export default function FoundResults({ pokemon }) {
+  return (
+    <div className="foundResultsContainer">
+      {pokemon.captured.capture === true ? (
+        <h1 className="textWhite">
+          You Caught a wild{" "}
+          {pokemon.shiny ? <span className="goldText">Shiny </span> : ""}
+          {pokemon?.pokeInfo.name.charAt(0).toUpperCase() +
+            pokemon?.pokeInfo.name.slice(1).toLowerCase()}
+          !
+        </h1>
+      ) : (
+        <h1 className="textWhite">
+          You Found a wild{" "}
+          {pokemon.shiny ? <span className="goldText">Shiny </span> : ""}{" "}
+          {pokemon?.pokeInfo.name.charAt(0).toUpperCase() +
+            pokemon?.pokeInfo.name.slice(1).toLowerCase()}
+          {pokemon.shiny ? "!" : ""}{" "}
+        </h1>
+      )}
+    </div>
+  );
+}
