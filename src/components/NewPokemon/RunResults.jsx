4157e8f2b9738d8160d9f5545d32866de06ad85c
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-14 10:34:50 -0800

    updated

---
+++ src/components/NewPokemon/RunResults.jsx
@@ -0,0 +1,38 @@
+import CaptureDisplay from "./CaptureDisplay";
+import { EmptyCard } from "./EmptyCard";
+import GameInfoDisplay from "./GameInfoDisplay";
+import PokeballAmountDisplay from "./PokeballAmountDisplay";
+import PokeBallDisplay from "./PokeBallDisplay";
+
+export default function RunResults({
+  catchMessage,
+  pokeBallCount,
+  setPokeBallCount,
+  greatBallCount,
+  setGreatBallCount,
+  ultraBallCount,
+  setUltraBallCount,
+}) {
+  return (
+    <>
+      <div className="resultsContainer">
+        <div className="foundResultsContainer">
+          <h1 className="textWhite">{catchMessage}</h1>
+        </div>
+
+        <div className="uiContainerRun">
+          <GameInfoDisplay />
+          <EmptyCard />
+          <PokeballAmountDisplay
+            pokeBallCount={pokeBallCount}
+            setPokeBallCount={setPokeBallCount}
+            greatBallCount={greatBallCount}
+            setGreatBallCount={setGreatBallCount}
+            ultraBallCount={ultraBallCount}
+            setUltraBallCount={setUltraBallCount}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
